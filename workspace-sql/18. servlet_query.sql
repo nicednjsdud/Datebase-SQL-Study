bf962363b51857ccf81598249809d3ba787d2858
nicednjsdud <nicednjsdud@naver.com>
2022-06-15 23:24:37 +0900

    query study

--- workspace-sql/18. servlet_query.sql
+++ workspace-sql/18. servlet_query.sql
@@ -45,8 +45,9 @@ CREATE TABLE MEMBER (
 
 
 INSERT INTO "MEMBER" (ID,PASS,NAME) VALUES ('EZEN','0824','이젠');
+INSERT INTO "MEMBER" (ID,PASS,NAME) VALUES ('bob','1234','정원영');
 SELECT * FROM MEMBER WHERE ID = 'EZEN' AND PASS ='0824';
-
+COMMIT;
 DROP TABLE board CASCADE CONSTRAINTS;
 CREATE TABLE board (
 		num NUMBER PRIMARY KEY ,
@@ -77,6 +78,10 @@ INSERT INTO board VALUES (seq_board_num.nextval, '오늘은 6월 2째주','월요일같은 
 		,'EZEN',sysdate,0);
 INSERT INTO board VALUES (seq_board_num.nextval, '2022년 절반이 감','어느덧 올해도 절반이 지나갑니다.'
 		,'EZEN',sysdate,0);
+INSERT INTO board VALUES (seq_board_num.nextval, '신논현역 분당성 개통','공사가 다 무리되어서 인도가 넓어졌으나 건물 공사가 한쪽에서..'
+		,'EZEN',sysdate,0);
+INSERT INTO board VALUES (seq_board_num.nextval, 'BoB님의 게시물입니다.','자바 spring 등 여러가지 공부를 하고있습니다.'
+		,'EZEN',sysdate,0);
 COMMIT;
 
 SELECT * FROM BOARD ORDER BY NUM DESC;
@@ -90,9 +95,23 @@ SELECT count(*) FROM board WHERE title LIKE '%오늘%';
 SELECT count(*) FROM board WHERE CONTENT LIKE '%월%';
 
 
+SELECT b.*,m.NAME FROM "MEMBER" m INNER JOIN BOARD b ON m.ID = b.ID 
+WHERE num = 3
+;
 
+-- 조회수 증가
+UPDATE BOARD SET VISITCOUNT = VISITCOUNT +1 WHERE num = 1;
+COMMIT;
 
+-- 게시물 수정
+UPDATE BOARD SET TITLE = '내일도 알고리즘 문제를 풀겠습니다.', CONTENT = 'DP문제를 마스터 하겠습니다' WHERE NUM = 8;
+COMMIT;
 
+-- 게시물 삭제
+DELETE FROM board WHERE num = 6;
+COMMIT;
+
+SELECT DECODE(COUNT(*),1,'true','false') AS result FROM T_MEMBER WHERE id= 'LEE';
 
 
 
