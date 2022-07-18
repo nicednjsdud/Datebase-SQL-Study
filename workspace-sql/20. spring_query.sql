8c6d3de1083113aa24c8d229c20229b54642768a
JeongWonYoung <nicednjsdud@naver.com>
2022-07-18 15:50:05 +0900

    Study database Query
    study database query with spring board

--- workspace-sql/20. spring_query.sql
+++ workspace-sql/20. spring_query.sql
@@ -10,3 +10,83 @@ INSERT INTO ADMIN.CUSTOM_ACCOUNT (ACCOUNTNO, CUSTNAME, BALANCE) VALUES('2022-07-
 COMMIT;
 
 SELECT * FROM CUSTOM_ACCOUNT ;
+
+SELECT * FROM t_member WHERE id = 'LEE' AND pwd = '0824';
+
+-- 게시판 테이블 생성
+DROP TABLE t_board CASCADE CONSTRAINTS;
+CREATE TABLE t_board(
+	articleNO number(10) PRIMARY KEY,
+	parentNO number(10),
+	title varchar2(500) NOT NULL,
+	content varchar2(4000),
+	imageFileName varchar2(100),
+	writeDate DATE DEFAULT sysdate NOT null,
+	id varchar2(20) NOT NULL,
+	CONSTRAINT fk_id FOREIGN KEY (id) references t_member(id)
+);
+
+INSERT INTO ADMIN.T_BOARD
+(ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
+VALUES(1, 0, '장마철', '장마철 입니다.', null, sysdate , 'LEE');
+
+INSERT INTO ADMIN.T_BOARD
+(ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
+VALUES(2, 0, 'TO Do List', '오늘의 할일입니다.', null, sysdate , 'BOB');
+
+INSERT INTO ADMIN.T_BOARD
+(ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
+VALUES(3, 2, '답변입니다.', '첫번째는 무엇인가요?', null, sysdate , 'LEE');
+
+INSERT INTO ADMIN.T_BOARD
+(ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
+VALUES(4, 0, '신사임당입니다', 'TEST입니다.', null, sysdate , 'SHIN');
+
+INSERT INTO ADMIN.T_BOARD
+(ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
+VALUES(5, 3, '답변입니다.', '알고리즘 공부입니다.', null, sysdate , 'BOB');
+
+INSERT INTO ADMIN.T_BOARD
+(ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
+VALUES(6, 2, '답변입니다.', '잘보고 있습니다..', null, sysdate , 'HONG');
+
+COMMIT;
+
+
+SELECT * FROM T_BOARD tb ;
+
+-- 글목록 조회
+SELECT LEVEL, ARTICLENO ,PARENTNO ,lpad(' ',4*(LEVEL-1)) || TITLE title, CONTENT ,WRITEDATE ,ID 
+FROM T_BOARD 
+START WITH parentNO = 0		-- 계층형 구조에서 최상위 로우(row)를 식별하는 조건 (즉 부모 글부터 시작)
+CONNECT BY PRIOR ARTICLENO =PARENTNO  --계층 구조가 어떻게 식으로 연결되는지 기술함
+ORDER siblings BY ARTICLENO DESC ;
+
+SELECT nvl(Max(articleNO),0) +1
+FROM t_board ;
+
+INSERT INTO T_BOARD (ArticleNO,TITLE,CONTENT,IMAGEFILENAME,id)
+VALUES ('7','금요일','내일 초복입니다',NULL,'LEE');
+
+ROLLBACK;
+
+-- 다중 파일 업로드
+DROP TABLE T_ImageFile CASCADE CONSTRAINTS;
+CREATE TABLE T_ImageFile(
+	imageFileNO number(10) PRIMARY KEY,
+	imageFileName varchar2(50),
+	regDate DATE DEFAULT sysdate,
+	articleNO number(10),
+	CONSTRAINT FK_articleNO FOREIGN KEY(articleNO) REFERENCES T_board(articleNO)
+	ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
---
+++ workspace-sql/21. spring-query.sql
@@ -0,0 +1,32 @@
+-- store 프로젝트 멤버 테이블
+DROP TABLE tb_shopping_member CASCADE CONSTRAINTS;
+CREATE TABLE tb_shopping_member (
+	member_id varchar2(20) PRIMARY KEY,
+	member_pw varchar2(30),
+	member_name varchar2(50),
+	member_gender varchar2(10),
+	hp1	varchar2(20),
+	hp2 varchar2(20),
+	hp3 varchar2(20),
+	smssts_yn	varchar2(20),
+	email1 varchar2(20),
+	email2 varchar2(20),
+	emailsts_yn varchar2(20),
+	zipcode varchar2(20),
+	roadaddress varchar2(500),
+	jibunaddress varchar2(500),
+	namujiaddress varchar2(500),
+	member_birth_y varchar2(20),
+	member_birth_m varchar2(20),
+	member_birth_d varchar2(20),
+	MEMBER_birth_gn varchar2(20),
+	joindate DATE DEFAULT sysdate,
+	del_yn varchar2(20) DEFAULT 'N'
+);
+
+INSERT INTO ADMIN.TB_SHOPPING_MEMBER
+(MEMBER_ID, MEMBER_PW, MEMBER_NAME, MEMBER_GENDER, HP1, HP2, HP3, SMSSTS_YN, EMAIL1, EMAIL2, EMAILSTS_YN, ZIPCODE, ROADADDRESS, JIBUNADDRESS, NAMUJIADDRESS, MEMBER_BIRTH_Y, MEMBER_BIRTH_M, MEMBER_BIRTH_D, MEMBER_BIRTH_GN, JOINDATE, DEL_YN)
+VALUES('bob', '0824', '정원영', '101', '010', '3456', '7890', 'Y', 'bob', 'gmail.com,non', 'N', '2201718', '경기도 화성시 기산동', '서경초등학교','1층', '1997', '3', '27', '2','2022-07-18', 'N'
+);
+
+SELECT decode(count(*),1,'true',0,'false') FROM TB_SHOPPING_MEMBER tsm WHERE MEMBER_ID ='bob'
