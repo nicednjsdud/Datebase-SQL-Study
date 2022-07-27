c961feb939e3d18efb24cd64c8de5c281f47e006
JeongWonYoung <nicednjsdud@naver.com>
2022-07-27 16:47:43 +0900

    Fix query to add t-board2 column
    fix column to add viewcounts

--- workspace-sql/20. spring_query.sql
+++ workspace-sql/20. spring_query.sql
@@ -14,39 +14,40 @@ SELECT * FROM CUSTOM_ACCOUNT ;
 SELECT * FROM t_member WHERE id = 'LEE' AND pwd = '0824';
 
 -- 게시판 테이블 생성
-DROP TABLE t_board CASCADE CONSTRAINTS;
-CREATE TABLE t_board(
+DROP TABLE t_board2 CASCADE CONSTRAINTS;
+CREATE TABLE t_board2(
 	articleNO number(10) PRIMARY KEY,
+	groupno number(10),
 	parentNO number(10) DEFAULT 0,
 	title varchar2(500) NOT NULL,
 	content varchar2(4000),
 	imageFileName varchar2(100),
 	writeDate DATE DEFAULT sysdate NOT null,
 	id varchar2(20) NOT NULL,
-	CONSTRAINT fk_id FOREIGN KEY (id) references t_member(id)
+	CONSTRAINT fk2_id FOREIGN KEY (id) references t_member(id)
 );
 
-INSERT INTO ADMIN.T_BOARD
+INSERT INTO ADMIN.T_BOARD2
 (ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
 VALUES(1, 0, '장마철', '장마철 입니다.', null, sysdate , 'LEE');
 
-INSERT INTO ADMIN.T_BOARD
+INSERT INTO ADMIN.T_BOARD2
 (ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
 VALUES(2, 0, 'TO Do List', '오늘의 할일입니다.', null, sysdate , 'BOB');
 
-INSERT INTO ADMIN.T_BOARD
+INSERT INTO ADMIN.T_BOARD2
 (ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
 VALUES(3, 2, '답변입니다.', '첫번째는 무엇인가요?', null, sysdate , 'LEE');
 
-INSERT INTO ADMIN.T_BOARD
+INSERT INTO ADMIN.T_BOARD2
 (ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
 VALUES(4, 0, '신사임당입니다', 'TEST입니다.', null, sysdate , 'SHIN');
 
-INSERT INTO ADMIN.T_BOARD
+INSERT INTO ADMIN.T_BOARD2
 (ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
 VALUES(5, 3, '답변입니다.', '알고리즘 공부입니다.', null, sysdate , 'BOB');
 
-INSERT INTO ADMIN.T_BOARD
+INSERT INTO ADMIN.T_BOARD2
 (ARTICLENO, PARENTNO, title, CONTENT, IMAGEFILENAME, WRITEDATE, ID)
 VALUES(6, 2, '답변입니다.', '잘보고 있습니다..', null, sysdate , 'HONG');
 
@@ -74,10 +75,10 @@ ROLLBACK;
 DROP TABLE T_ImageFile CASCADE CONSTRAINTS;
 CREATE TABLE T_ImageFile(
 	imageFileNO number(10) PRIMARY KEY,
-	imageFileName varchar2(50),
+	imageFileName varchar2(2000),
 	regDate DATE DEFAULT sysdate,
 	articleNO number(10),
-	CONSTRAINT FK_articleNO FOREIGN KEY(articleNO) REFERENCES T_board(articleNO)
+	CONSTRAINT FK_articleNO FOREIGN KEY(articleNO) REFERENCES T_board2(articleNO)
 	ON DELETE CASCADE
 );
 
@@ -92,10 +93,131 @@ INSERT INTO ADMIN.TB_SHOPPING_MEMBER
 VALUES('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', sysdate, 'N'
 );
 
+DELETE from t_board WHERE articleNO = 7;
+ROLLBACK;
+
+DELETE FROM T_IMAGEFILE WHERE articleNO = 7 AND IMAGEFILENO = 16;
+
+DELETE FROM t_imageFile WHERE articleNo = 24 AND IMAGEFILENO = 19
+
+COMMIT;
+
+-- 글 그룹 조회하기
+SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC;
+
+-- 각각의 행번호를 동시에 조회하기
+
+SELECT rowNum AS recnum,  groupNO
+from(
+SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC
+);
+
+-- rowNum 필드값을 이용해서 1에서 10까지의 레코드만 조회하기
+
+SELECT recNum, groupNO
+FROM (
+	SELECT rowNum AS recnum,  groupNO
+from(
+SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC
+)
+)
+WHERE recNum BETWEEN 1 AND 10
+;
+
+SELECT * FROM T_BOARD2
+WHERE GROUPNO IN(
+	SELECT GROUPNO 
+	FROM (
+		SELECT recNum, groupNO
+		FROM (
+			SELECT rowNum AS recnum,  groupNO
+				from(
+					SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC
+					)
+			)
+		WHERE recNum BETWEEN 1 AND 10	
+		)
+)
+;
+
+SELECT groupNO,LEVEL AS lvl, articleno,parentno,title,id,writedate
+FROM (
+		SELECT * FROM T_BOARD2
+			WHERE GROUPNO IN(
+				SELECT GROUPNO 
+				FROM (
+					SELECT recNum, groupNO
+					FROM (
+						SELECT rowNum AS recnum,  groupNO
+							from(
+								SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC
+								)
+						)
+					WHERE recNum BETWEEN 1 AND 10	
+					)
+			)		
+	)
+	START WITH parentNO = 0		
+	CONNECT BY PRIOR ARTICLENO = PARENTNO  
+	ORDER siblings BY ARTICLENO DESC;
 
 
+SELECT GROUPNO ,LVL ,articleNO,parentNO,title,id,writedate
+from(
+	SELECT groupNO,LEVEL AS lvl, articleno,parentno,title,id,writedate
+	FROM (
+			SELECT * FROM T_BOARD2
+				WHERE GROUPNO IN(
+					SELECT GROUPNO 
+					FROM (
+						SELECT recNum, groupNO
+						FROM (
+							SELECT rowNum AS recnum,  groupNO
+								from(
+									SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC
+									)
+							)
+						WHERE recNum BETWEEN 1 AND 10	
+						)
+				)		
+		)
+		START WITH parentNO = 0		
+		CONNECT BY PRIOR ARTICLENO = PARENTNO  
+		ORDER siblings BY ARTICLENO DESC
+)
+;	
 
+SELECT * FROM (
+	SELECT GROUPNO ,LVL ,articleNO,parentNO,title,id,writedate
+	from(
+		SELECT groupNO,LEVEL AS lvl, articleno,parentno,lpad(' ', 4*(LEVEL-1))|| TITLE title,id,writedate
+		FROM (
+				SELECT * FROM T_BOARD2
+					WHERE GROUPNO IN(
+						SELECT GROUPNO 
+						FROM (
+							SELECT recNum, groupNO
+							FROM (
+								SELECT rowNum AS recnum,  groupNO
+									from(
+										SELECT DISTINCT groupNO FROM T_BOARD2 tb ORDER BY GROUPNO DESC
+										)
+								)
+							WHERE recNum BETWEEN 1 AND 10	
+							)
+					)		
+			)
+			START WITH parentNO = 0		
+			CONNECT BY PRIOR ARTICLENO = PARENTNO  
+			ORDER siblings BY ARTICLENO DESC
+	)
+)
+;
+SELECT count(DISTINCT GROUPNO) FROM T_BOARD2 tb ;
 
+ALTER TABLE T_BOARD2 ADD viewcounts number(10) DEFAULT 0;
+ALTER TABLE T_BOARD2 DROP COLUMN viewcounts;
+ALTER TABLE T_BOARD2 ALTER COLUMN viewcounts DEFAULT 0;
 
-
-
+-- 조회수 업데이트
+UPDATE T_BOARD2 SET viewcounts = viewcounts + 1 WHERE ARTICLENO = 2;
